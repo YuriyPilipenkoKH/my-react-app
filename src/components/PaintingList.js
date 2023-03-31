4db060122763b2496a167c8ae85f1dcfbc9eff7e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-03-31 17:40:29 +0300

    first

--- src/components/PaintingList.js
+++ src/components/PaintingList.js
@@ -1,27 +1,31 @@
-import Painting from "./Painting"
-import PropTypes from "prop-types";
+import Painting from './Painting';
+import PropTypes from 'prop-types';
 
-function PaintingList ({items}) {
-
+function PaintingList({ items }) {
+  return (
     <ul>
-    {items.map(item => (
-        <li key = {item.id}>
-            <Painting
-        
-        url = {item.url}
-        title = {item.title}  
-        authorName = {item.author.tag}
-        profileUrl = {item.author.url}
-        price = {item.price}
-        quantity= {item.quantity}   />
+      {items.map(item => (
+        <li key={item.id}>
+          <Painting
+            imageUrl={item.url}
+            title={item.title}
+            author={item.author.tag}
+            profileUrl={item.author.url}
+            price={item.price}
+            quantity={item.quantity}
+          />
         </li>
-    ))}
+      ))}
     </ul>
+  );
 }
 
-PaintingList.PropTypes = {
-    item: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired, 
-    }))
-}
-export default PaintingList
+PaintingList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  ),
+};
+
+export default PaintingList;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function Section({title,children}) {
-    return( <div>
-        {title && <h2>{title}</h2>}
-        <p> {React.Children.count(children)}</p>
-        {children}
-    </div>)
+function Section({ title, children }) {
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      {children}
+    </div>
+  );
 }
 
 Section.propTypes = {
-    title: PropTypes.string,
-    // children: PropTypes.node,
-}
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
 
-export default  Section
+export default Section;
