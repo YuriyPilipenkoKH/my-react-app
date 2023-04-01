89f16421485b6a6df272fd855a8652efa809c94f
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-01 22:36:03 +0300

    upcomingEvents

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,5 +1,6 @@
 import css from './EventBoard.module.css'
 
-export const EventBoard = () => {
+export const EventBoard = ({events}) => {
+    console.log(events);
     return <div className={css.eventBoard}>Event cards</div>
 }
