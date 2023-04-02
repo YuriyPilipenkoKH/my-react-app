ab4d7855a67391e45e94ba29738393aa9aa06a32
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-02 13:07:42 +0300

    2 classnames

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,9 +1,25 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'  
+import css from './Event.module.css'
+import { FaMapMarkerAlt, FaUserAlt, FaUserTie, FaCalendarAlt, FaClock } from 'react-icons/fa';
+import { FcCalendar, FcClock } from 'react-icons/fc';
+
 
 export const Event = ({name,location,speaker,type,start,end}) => {
     return (
-    <div className="event">
-        <h2 className="title">rock</h2>
+    <div className={css.event}>
+        <h2 className={css.title}>{name}</h2>
+        <p className={css.info}>
+            <FaMapMarkerAlt className={css.icon} size={16}/>
+            {location}</p>
+        <p className={css.info}> 
+             <FaUserTie className={[css.icon, css.user ].join(' ')}  size={16}/>
+            {speaker}</p>
+        <p className={css.info}>
+             <FcCalendar className={css.icon} size={16}/>
+            {start}</p>
+        <p className={css.info}>
+             <FcClock className={css.icon} size={16}/>
+            duration</p>
 
     </div>)
 };
