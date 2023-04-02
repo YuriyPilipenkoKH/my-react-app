e185abb07a311df6053391a304e5bd58802c3046
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-02 21:52:17 +0300

    container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,13 @@
 import { PageTitle } from './PageTitle/PageTitle';
 import { EventBoard } from './EventBoard/EventBoard';
 import upcomingEvents from '../upcoming-events.json';
+import { Container } from './App.styled';
 
 export const App = () => {
   return (
-    <>
+    <Container>
       <PageTitle text={'24th Core Worlds Coalition Conference'} />
       <EventBoard events={upcomingEvents} />
-    </>
+    </Container>
   );
 }; 
