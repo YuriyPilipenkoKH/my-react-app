d2e6909d286ec95264d73876f36a07928ce709fe
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-02 13:48:36 +0300

    duration

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -2,11 +2,15 @@ import PropTypes from 'prop-types'
 import css from './Event.module.css'
 import { FaMapMarkerAlt, FaUserAlt, FaUserTie, FaCalendarAlt, FaClock } from 'react-icons/fa';
 import { FcCalendar, FcClock } from 'react-icons/fc';
-import {formatEventStart } from 'utils/formatEventStart.js'
+import {formatEventStart } from 'utils/formatEventStart'
+import {formatEventDuration} from 'utils/formatEventDuration'
 
 
 export const Event = ({name,location,speaker,type,start,end}) => {
+
     const fomatedStart = formatEventStart(start)
+    const duration = formatEventDuration(start, end)
+
     return (
     <div className={css.event}>
         <h2 className={css.title}>{name}</h2>
@@ -21,7 +25,7 @@ export const Event = ({name,location,speaker,type,start,end}) => {
             {fomatedStart}</p>
         <p className={css.info}>
              <FcClock className={css.icon} size={16}/>
-            duration</p>
+            {duration}</p>
 
     </div>)
 };
