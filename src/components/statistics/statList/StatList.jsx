f54cbfda00975f5843c592d1ec38dbfc9afe1671
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-20 12:34:09 +0300

    k

--- src/components/statistics/statList/StatList.jsx
+++ src/components/statistics/statList/StatList.jsx
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types';
 import { Item } from './StatList.syled.jsx';
 
+function randomColor() {
+   
+  return  ('#'+ Math.floor(Math.random()*16777215).toString(16))
+}   ;
+
+
 export const StatList = ({id, label, percentage}) => {
     return (
         <>
-        <Item className="item" >
+        <Item className="item" style= {{backgroundColor: randomColor() }}>
         <span className="label" >{label}</span>
         <span className="percentage">{percentage}%</span>
         </Item>
--- src/components/statistics/statList/StatList.syled.jsx
+++ src/components/statistics/statList/StatList.syled.jsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 
-let randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 export const Item = styled.li`
   width: 100%;
@@ -9,6 +8,10 @@ export const Item = styled.li`
   flex-direction: column;
   gap: 8px;
   padding: 20px 0;
-  border-right: 1px solid #999 ;
-  background-color: #${randomColor} ;
+  border-right: 1px solid #000 ;
+
+  :last-child {
+  border-right: none;
+}
+
 `
