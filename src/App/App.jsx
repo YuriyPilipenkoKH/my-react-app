4c05db00b668eb5656ebed3649af06c76d2abef2
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-21 13:38:49 +0300

    container

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
 import user from '../data/user.json'
 import statistics from '../data/statistics.json';
 import friends from '../data/friends.json'
-
+import { Container } from 'components/container/Container';
 import { Profile } from '../components/profile/Profile'
 import { Statistics } from '../components/statistics/statistics'
 import {FriendsList} from '../components/friendlist/FriendList'
@@ -11,10 +11,10 @@ import {FriendsList} from '../components/friendlist/FriendList'
 export const App = () => {
 
   return (
-    <div className='wrapper'>
+    <Container className='wrapper'>
       <Profile user={user}></Profile>
       <Statistics statistics={statistics} title="Upload stats"></Statistics>
       <FriendsList friends={friends}></FriendsList>
-    </div>
+    </Container>
   );
   };
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import { MainContainer } from './Container.styled';
+
+export const Container =({ children })  => {
+    return <MainContainer className='container'>{children}</MainContainer>
+}
+
+Container.propTypes = {
+    children: PropTypes.node,
+  };
+  
