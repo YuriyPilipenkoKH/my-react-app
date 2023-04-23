d3f6036b568375733a2c5ddbe44db47b8e66f540
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-23 14:48:05 +0300

    title

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,7 @@
 import { Section } from "../components/section/Section";
 import { FeedbackOptions } from "../components/FeedbackOptions/FeedbackOptions";
+import { colorPickerOptions } from "../data/options";
+import { ColorPicker } from "../components/colorPicker/colorPicker";
 
 let state = {
   good: 0,
@@ -9,9 +11,15 @@ let state = {
 
 export const App = () => {
     return (
-      <Section title ="Please leave feedback" >
+  
+    
+<>
+<Section title ="Please leave feedback" >
          <FeedbackOptions state ={state}></FeedbackOptions>
- 
+        
       </Section>
+      <ColorPicker options= {colorPickerOptions}></ColorPicker>
+</>
+     
     );
   };
--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -2,11 +2,10 @@ import PropTypes from 'prop-types'
 import { Title } from '../Title/Title'
 
 
-export const Section = ({title}) => {
-
+export const Section = (props) => {
    return (
    <section className='section'>
-    <Title text={title}></Title>
+    <Title text={props.title}></Title>
 
     </section>)
 }
