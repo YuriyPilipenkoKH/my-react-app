f378949cfa2b8231fe2b2698aabf83d8dc43545e
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-23 15:52:09 +0300

    sh

---
+++ src/constants/theme.js
@@ -0,0 +1,23 @@
+export const theme = Object.freeze({
+    colors: {
+      white: '#ffffff',
+      black: '#010101',
+      green: '#12e319',
+      red: '#B92F2C',
+      blue: '#2196f3',
+      lauren: '#A4C495',
+      teal: '#008080',
+      grey: '#f7f7f7',
+      orange: '#ffa500',
+      purple: '#9797e8',
+      beige: '#f5f5dc',
+      adelyn: '#3C204B99',
+      chamomile: '#E9DBC4',
+      primaryText: '#212121',
+      secondaryText: '#757575',
+    },
+    fontSize: value => `${4 * value}px`,
+    spacing: value => `${4 * value}px`,
+
+    shadow:  "1rem 1rem 2rem rgb(65, 64, 64), -1rem -1rem 2rem rgb(82, 81, 81)",
+  });
