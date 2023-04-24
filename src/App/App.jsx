c227f2c0b412d937155047b7a4f10562cc0fe1aa
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-24 12:28:32 +0300

    container

---
+++ src/App/App.jsx
@@ -0,0 +1,22 @@
+import {Component} from 'react'
+import { Container } from '../components/container/Container.styled';
+import { Section } from "../components/section/Section";
+
+
+
+export class App extends Component {
+render() {
+
+  return (
+    <Container>
+   <Section title ="Main" children>
+   
+
+    </Section>
+
+    </Container>
+
+
+   
+    )}  
+};
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.div`
+    display: grid;
+    justify-items: center;
+    align-items: center;
+    grid-gap: 50px;
+    background-color: #cad9d9;
+    padding: 16px;
+    margin: 0 auto;
+`
