242bf138210472e6ec047c75b446e06e5f2efa3b
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-24 21:09:43 +0300

    juk

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,14 @@
 import { Component } from "react";
 import { Container } from "../components/container/Container.styled";
 import { LoginForm } from "../components/LoginForm/LoginForm";
+import { ProductReviewForm } from "../components/ProductReviewForm/ProductReviewForm";
 
 export class App extends Component {
   render() {
     return (
       <Container>
       <LoginForm/>
+      <ProductReviewForm/>
       </Container>
     )
   }
--- src/components/LoginForm/LoginForm.styled.jsx
+++ src/components/LoginForm/LoginForm.styled.jsx
@@ -4,25 +4,39 @@ import { Formik, Form, Field} from 'formik';
 export const FormCard = styled(Form)`
     display: flex;
     flex-direction: column;
-    gap: 20px;
+    gap: 10px;
+    border: 4px solid #777;
+    padding: 16px;
 
     >label {
         display: flex;
         flex-direction: column;
 
         >input {
-            font-size: 22px;
+            font-size: 14px;
             padding: 8px;
+            outline: none;
+           
         }
     }
 
     >button {
-        font-size: 22px;
+        font-size: 18px;
+        font-weight: 700;
         padding: 8px 16px;
-        max-width: 160px;
-        border-radius: 12px;
+        max-width: 120px;
+        border-radius: 10px;
         border: transparent;
+        color: #333;
         background-color: #2196f3;
         cursor: pointer;
+        transition: 400ms ease-in-out;
+
+        :hover,
+        :focus {
+            background-color: #17a;
+            color: #eee;
+
+        }
     }
 `
