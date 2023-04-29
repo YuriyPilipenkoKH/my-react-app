8c5e0908db6fb375f083172ebc3e1de58c8d6de7
Yuriy Pilipenko <yurik2061@gmail.com>
2023-04-29 09:02:42 +0300

    hide

--- src/components/section/Section.styled.jsx
+++ src/components/section/Section.styled.jsx
@@ -10,99 +10,99 @@ export const Sect  = styled.section`
 
 `
 
-const slideOut= keyframes`
- 0% {
-    left: 0;
-  }
-  75% {
-    left: 2000px;
-    max-height: 10rem;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    border-width: 1px;
-    overflow: hidden;
-  }
-  100% {
-    left: 2000px;
-    max-height: 0;
-    padding-top: 0;
-    padding-bottom: 0;
-    border-width: 0;
-    overflow: hidden;
-  }
-`
+// const slideOut= keyframes`
+//  0% {
+//     left: 0;
+//   }
+//   75% {
+//     left: 2000px;
+//     max-height: 10rem;
+//     padding-top: 1rem;
+//     padding-bottom: 1rem;
+//     border-width: 1px;
+//     overflow: hidden;
+//   }
+//   100% {
+//     left: 2000px;
+//     max-height: 0;
+//     padding-top: 0;
+//     padding-bottom: 0;
+//     border-width: 0;
+//     overflow: hidden;
+//   }
+// `
  
-const minimize = keyframes`
+// const minimize = keyframes`
   
-  0% {
-    max-height: 10rem;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    border-width: 1px;
-    overflow: hidden;
-  }
-  100% {
-    max-height: 0;
-    padding-top: 0;
-    padding-bottom: 0;
-    border-width: 0;
-    overflow: hidden;
-  }
-`
-const drop = keyframes`
-0% {
-    opacity: 0;
-    transform: translateY(var(--distY));
-  }
-  100% { 
-    opacity: 1;
-    transform: translateY(0);
-  } 
+//   0% {
+//     max-height: 10rem;
+//     padding-top: 1rem;
+//     padding-bottom: 1rem;
+//     border-width: 1px;
+//     overflow: hidden;
+//   }
+//   100% {
+//     max-height: 0;
+//     padding-top: 0;
+//     padding-bottom: 0;
+//     border-width: 0;
+//     overflow: hidden;
+//   }
+// `
+// const drop = keyframes`
+// 0% {
+//     opacity: 0;
+//     transform: translateY(var(--distY));
+//   }
+//   100% { 
+//     opacity: 1;
+//     transform: translateY(0);
+//   } 
 
-`
+// `
 
 
-const swing = keyframes`
-0% {
-    opacity: 0;
-    transform: 
-      translateX-(var(--distX))
-      translateY(var(--distY));
-  }
-  25% {
-    transform:
-      translateX(var(--distX))/2
-      translateY(var($distY))/2;
-  }
-  50% {
-    transform:
-      translateX-(var(--distX)/4)
-      translateY(var(--distY)/4);
-  }
-  75% {
-    transform:
-      translateX-(var(--distX))/8
-      translateY(var(--distY))/8;
-  }
-  100% { 
-    opacity: 1;
-    transform: 
-      translateX(0)
-      translateY(0);
-  }
-`
+// const swing = keyframes`
+// 0% {
+//     opacity: 0;
+//     transform: 
+//       translateX-(var(--distX))
+//       translateY(var(--distY));
+//   }
+//   25% {
+//     transform:
+//       translateX(var(--distX))/2
+//       translateY(var($distY))/2;
+//   }
+//   50% {
+//     transform:
+//       translateX-(var(--distX)/4)
+//       translateY(var(--distY)/4);
+//   }
+//   75% {
+//     transform:
+//       translateX-(var(--distX))/8
+//       translateY(var(--distY))/8;
+//   }
+//   100% { 
+//     opacity: 1;
+//     transform: 
+//       translateX(0)
+//       translateY(0);
+//   }
+// `
    
 
-const bounce =keyframes`
-0% {
-    opacity: 0;
-    transform: translateY(var(--distY));
-  }
-  100% { 
-    opacity: 1;
-    transform: translateY(0);
-  }
-`
+// const bounce =keyframes`
+// 0% {
+//     opacity: 0;
+//     transform: translateY(var(--distY));
+//   }
+//   100% { 
+//     opacity: 1;
+//     transform: translateY(0);
+//   }
+// `
 
 
 
