4bf5e2811954ba43dfb65fd2674d3bb746164985
Yuriy Pilipenko <yurik2061@gmail.com>
2023-05-03 15:26:10 +0300

    pixabay

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,20 +1,51 @@
 import {Component} from 'react'
+import axios from "axios";
 import { Container } from '../components/container/Container';
 // import { Section } from "../components/section/Section";
 // import Button from '../components/buttons/buttons';
 import { Searchbar } from '../components/Searchbar/Searchbar';
 
 
+
 export class App extends Component {
+  state = {
+    query: '',
+  } 
+
+imgSearch =(query) => {
+  console.log('this.imgSearch');
+  console.log(query);
+
+  this.setState({  query: query });
+  this.fetchImages(query)
+  
+}
+
+componentDidMount(prevProps, prevState) {
+ 
+}
+
+fetchImages =(query) => {
+  const KEY = '34491623-5c4ef369d6c59f62bc483440c';
+  const BASE_URL = 'https://pixabay.com/api';
+  const page = 1
+  const perPage = 20
+
+  fetch(`${BASE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
+  .then(res => res.json())
+  .then(console.log)
+
+}
+
 
 render() {
 
   return (
     <Container>
-  <Searchbar/>
+  <Searchbar onSubmit={this.imgSearch}/> 
    
     </Container>
 
    
     )}  
-};
+};//
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -7,14 +7,19 @@ export class Searchbar extends Component {
     }
 
     handleChange = e => {
+        const  searchQuery  = e.target.value.trim();
+        // console.log(e);
+        console.log(searchQuery);
+        this.setState({  query: searchQuery });
+        
 
-    }
+    }// this.setState(prevState => ({ query: prevState.query + 1 }));
 
     handleSubmit = e => {
         e.preventDefault();
-        // this.props.onSubmit(this.state);
+        this.props.onSubmit(this.state.query);
 
-        this.resetForm();
+        // this.resetForm();
     }
 
     resetForm = () => {
@@ -25,7 +30,9 @@ export class Searchbar extends Component {
   render() {
     return (
       <SearchHeader className="searchbar">
-     <form onSubmit={this.handleSubmit} className="form">
+     <form
+      onSubmit={this.handleSubmit} 
+      className="form">
     <button type="submit" className="button">
       <span className="button-label">Search</span>
     </button>
@@ -36,7 +43,7 @@ export class Searchbar extends Component {
       className="input"
       type="text"
       autoComplete="off"
-    //   autoFocus
+      autoFocus
       placeholder="Search images and photos"
       onChange={this.handleChange}
     />
